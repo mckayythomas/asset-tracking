bbc6b2646ba30fe4383ee0db57c32be0574b8411
Miguel Angel López Guzmán <lop18028@byui.edu>
2023-06-23 18:04:00 +0000

    Start Typescript and GraphQL branch.
    Created the new branch that will be used to develop the app with Typescript and GraphQL.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# asset-tracking
+# Asset-Tracking API
+
+> **Note:**
+This version of the API is developed with TypeScript and GraphQL.
--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/users');
-
-router.delete('/', userController.deleteUser)
-
-module.exports = router
