589d078d429be6fd9e93ef681e4552beceaa9905
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-06-30 11:35:09 -0400

    Update to add asset resolvers, schemas, and models

---
+++ src/models/asset.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+const { Schema } = mongoose;
+
+// Define Mongoose address schema
+const addressSchema = new Schema({
+    street: { type: String, required: true },
+    town: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+    country: { type: String, required: true },
+});
+
+// Define  Mongoose asset schema
+const assetSchema = new Schema({
+    assetId: { type: String, unique: true, required: true },
+    serialNumber: { type: String, required: true },
+    brand: { type: String, required: true },
+    purchaseDate: { type: Date, required: true },
+    model: { type: String, required: true },
+    modelNumber: { type: String, required: true },
+    purchasePrice: { type: Number, required: true },
+    image: { type: String, required: true },
+    physicalDescription: { type: String, required: true },
+    status: { type: String, required: true },
+    condition: { type: String, required: true },
+    building: { type: String, ref: 'Building', required: true },
+    department: { type: String, ref: 'Department', required: true },
+    user: { type: String, ref: 'User', required: true },
+
+}, {
+    collection: 'asset',
+}
+);
+
+const Asset = mongoose.model('Asset', assetSchema);
+
+export {Asset};
