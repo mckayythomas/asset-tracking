3f522324183acb158cf8578573c62cdf99acee36
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-07-11 10:16:10 -0400

    Update tests

--- __mocks__/google.ts
+++
@@ -1,7 +0,0 @@
-// __mocks__/google.ts
-export const verify = jest.fn().mockImplementation(() => {
-    return Promise.resolve({
-      userId: 'fakeUserId',
-      email: 'fakeemail@gmail.com',
-    });
-  });
