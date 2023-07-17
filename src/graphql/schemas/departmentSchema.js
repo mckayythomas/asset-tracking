cdb857f553c14766088f8f64349ec2fc7f3623c4
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-07-16 22:03:06 -0400

    Delete departmentSchema.js

--- src/graphql/schemas/departmentSchema.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.departmentSchema = void 0;
-var departmentSchema = "\n    type Department {\n        _id: ID!\n        departmentId: String!\n        name: String!\n\n        \"The location associated with this department.\"\n        location: Location!\n        head: String!\n        employeesCount: Int!\n        description: String!\n    }\n";
-exports.departmentSchema = departmentSchema;
