d1bd6fcf0f3e5ef5eace0bd679b16c4e62fab90b
Josh Liddiard <joshliddiard15@gmail.com>
2023-07-17 16:01:04 -0600

    Tests functional for all routes

---
+++ src/models/building.ts
@@ -0,0 +1,14 @@
+import { isRequiredArgument } from 'graphql';
+import mongoose, { Schema, Document } from 'mongoose';
+
+const buildingSchema: Schema = new Schema({
+    buildingId: { type: String, required: true },
+    locationId: { type: String, required: true },
+    name: { type: String, required: true },
+    floors: { type: Number, required: true },
+    departments: [{ type: String, required: true }],
+  });
+
+const Building = mongoose.model('Building', buildingSchema, 'building');
+
+export {Building};
