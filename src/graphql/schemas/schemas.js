5ebd130e1fb3f8245c89426db200f800e73ab094
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-07-16 22:03:23 -0400

    Delete schemas.js

--- src/graphql/schemas/schemas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDefs = void 0;
-var merge_1 = require("@graphql-tools/merge");
-var departmentSchema_1 = require("./departmentSchema");
-var locationSchema_1 = require("./locationSchema");
-var typeDefs = (0, merge_1.mergeTypeDefs)([locationSchema_1.locationSchema, departmentSchema_1.departmentSchema]);
-exports.typeDefs = typeDefs;
